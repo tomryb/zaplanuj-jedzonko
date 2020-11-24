631724a8e692367496d8ab22485615479989a529
tomryb <1rybak.tomasz@gmail.com>
2020-11-24 17:20:45 +0100

    Updates

---
+++ precache-manifest.8ee1fe70e64b7158b42662fcc2fe31bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5941da66d4be988e065221306206ac",
+    "url": "/zaplanuj-jedzonko/index.html"
+  },
+  {
+    "revision": "e98df0c03245c14d7fac",
+    "url": "/zaplanuj-jedzonko/static/css/main.a6731bb9.chunk.css"
+  },
+  {
+    "revision": "82af50be1daf90fadefa",
+    "url": "/zaplanuj-jedzonko/static/js/2.34ef0268.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/zaplanuj-jedzonko/static/js/2.34ef0268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98df0c03245c14d7fac",
+    "url": "/zaplanuj-jedzonko/static/js/main.85a16d58.chunk.js"
+  },
+  {
+    "revision": "27c91269a9a98dff3f28",
+    "url": "/zaplanuj-jedzonko/static/js/runtime-main.5716dd7f.js"
+  }
+]);
